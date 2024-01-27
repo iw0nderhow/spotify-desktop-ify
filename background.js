81e9398c8693a912c45a7d806e4368bfe9cced3f
iw0 <gh-projects@heychris.eu>
2024-01-27 21:15:17 +0100

    add support for intl-* URLs

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ browser.webRequest.onBeforeRequest.addListener(
   ({ url }) => {
     const [fullMatch, type, identifier] =
       url.match(
-        /open\.spotify\.com\/(track|album|artist|playlist|concert|episode|show|user)\/([^\&\#\/\?]+)/i
+        /open\.spotify\.com\/(?:intl-[a-z]{2}\/)?(track|album|artist|playlist|concert|episode|show|user)\/([^\&\#\/\?]+)/i
       ) || [];
 
     return { redirectUrl: `spotify:${type}:${identifier}` };
@@ -11,7 +11,11 @@ browser.webRequest.onBeforeRequest.addListener(
     urls: ["*://open.spotify.com/track/*", "*://open.spotify.com/album/*",
   "*://open.spotify.com/artist/*", "*://open.spotify.com/playlist/*",
   "*://open.spotify.com/concert/*", "*://open.spotify.com/episode/*",
-  "*://open.spotify.com/show/*", "*://open.spotify.com/user/*"],
+  "*://open.spotify.com/show/*", "*://open.spotify.com/user/*",
+  "*://open.spotify.com/intl-*/track/*", "*://open.spotify.com/intl-*/album/*",
+  "*://open.spotify.com/intl-*/artist/*", "*://open.spotify.com/intl-*/playlist/*",
+  "*://open.spotify.com/intl-*/concert/*", "*://open.spotify.com/intl-*/episode/*",
+  "*://open.spotify.com/intl-*/show/*", "*://open.spotify.com/intl-*/user/*"],
     types: [
       "main_frame",
       "xmlhttprequest"]
